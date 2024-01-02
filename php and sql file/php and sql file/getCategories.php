4daf46c03f9a622b0f06bfe2e5d68371f48a8187
Zakaria Damaj <139499220+Zakaria-Damaj@users.noreply.github.com>
2024-01-02 21:32:19 +0200

    Add files via upload

---
+++ php and sql file/php and sql file/getCategories.php
@@ -0,0 +1,29 @@
+<?php
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+// Connect to the database
+$con = mysqli_connect("localhost", "root", "", "restaurant_db");
+
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+// Fetch categories from the database
+$query = "SELECT * FROM category";
+if ($result = mysqli_query($con,$query))
+  {
+   $emparray = array();
+   while($row =mysqli_fetch_assoc($result))
+       $emparray[] = $row;
+
+  echo(json_encode($emparray));
+  // Free result set
+  mysqli_free_result($result);
+  mysqli_close($con);
+}
+
+?> 	
+
